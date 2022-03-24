169ea8e5d0508d0c745f85bc2d30e90f469f484d
ponkio-o <29038315+ponkio-o@users.noreply.github.com>
2022-03-24 12:13:52 +0900

    FIX: script

--- script/check_branch.sh
+++ script/check_branch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if git ls-remote --exit-code --heads origin "$GITHUB_REF_NAME"; then
-  LATEST_SHA1_IN_CURRENT_REMOTE_BRANCH=$(git rev-parse "origin/$GITHUB_REF_NAME")
-  echo "COMMIT_HASH: ${COMMIT_HASH}"
-  echo "LATEST_SHA1: ${LATEST_SHA1_IN_CURRENT_REMOTE_BRANCH}"
-  echo "REF_NAME   : ${GITHUB_REF_NAME}"
-  if [[ "${COMMIT_HASH}" == "${LATEST_SHA1_IN_CURRENT_REMOTE_BRANCH}" ]]; then
+  DEFAULT_BRANCH_LATEST_SHA1=$(git rev-parse "origin/$GITHUB_REF_NAME")
+  FEATURE_BRANCH_LATEST_SHA1=$(git rev-parse "origin/$FEATURE_BRANCH_NAME")
+  echo "DEFAULT_BRANCH_LATEST_SHA1: ${DEFAULT_BRANCH_LATEST_SHA1}"
+  echo "FEATURE_BRANCH_LATEST_SHA1: ${FEATURE_BRANCH_LATEST_SHA1}"
+  echo "GITHUB_REF_NAME           : ${GITHUB_REF_NAME}"
+  if [[ "${DEFAULT_BRANCH_LATEST_SHA1}" == "${FEATURE_BRANCH_LATEST_SHA1}" ]]; then
     echo "Found current branch $GITHUB_REF_NAME is update to date, ready to run terraform."
   else
     echo "Skip to run terraform on old commit ${COMMIT_HASH} in current branch $GITHUB_REF_NAME."
